10e816ab757cc12c7b615b54ead415ecd1444768
Daniel Dawson <dannyldawson@gmail.com>
2019-12-10 16:49:55 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
+import { link } from "fs";
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map(movie => (
+      <div>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map(genre => (
+            <li>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    ));
+  };
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
